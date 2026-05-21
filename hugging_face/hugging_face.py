cebead3a170fcd732c59da222df9fdb100adebce
Atharv Agarwal <atharv20agarwal@gmail.com>
2026-05-22 03:10:59 +0530

    feat: hugging face image classifications

---
+++ hugging_face/hugging_face.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from PIL import Image
+from transformers import pipeline
+import requests
+import os
+
+load_dotenv()
+
+hf_token = os.getenv("HF_TOKEN")
+
+classifier = pipeline("image-classification")
+
+url = "https://i.pinimg.com/736x/cd/f1/35/cdf1350b3603ec01affbfa933bb6e79b.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+predictions = classifier(image)
+
+print("Image classification results:\n")
+for prediction in predictions:
+    print(f"- {prediction['label']}: {prediction['score']:.2f}")
